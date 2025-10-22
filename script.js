69c0a1407a9f1ce7db050a7ab26ab86955341156
wolf op <wolfop025@gmail.com>
2025-10-22 15:38:37 +0530

    Project Functionality Maximum complete

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
 
-
+let totalDollar = 0.00;
 const AddItemBTN = document.getElementById("AddItemButton");
 const contentInp = document.getElementById("content");
 const amountInp = document.getElementById("amount");
+const totalMoney = document.getElementById("totalMoney");
+
 
 AddItemBTN.addEventListener("click", function(){
-    let inpVal1 = contentInp.value;
-    let inpVal2 = parseFloat(amountInp.value);
+    let inpVal1 = contentInp.value.trim();
+    let inpVal2 = amountInp.value;
+    let trimVal2 = inpVal2.trim();
+    let floteVal2 = parseFloat(trimVal2).toFixed(2);
+    let finalVal2 = Number(floteVal2);
 
-    if(!inpVal1 && !inpVal2){
+    if(!inpVal1 || inpVal1 === "" || isNaN(finalVal2)){
         alert("Fill Your Bill.");
         return false;
     }else{
+        contentInp.value = "";
+        amountInp.value = "";
+        totalDollar = totalDollar + finalVal2;
         const p = document.createElement("p");
         const aa = document.createElement("aa");
-        aa.textContent = `${inpVal1 + ": $" + inpVal2.toFixed(2)}`;
+        aa.setAttribute("doller", finalVal2);
+        aa.textContent = `${inpVal1 + ": $" + finalVal2}`;
         const bb = document.createElement("bb");
-        bb.textContent = "DELETE";
+        bb.classList.add("delBTN");
+        const delIcon = document.createElement("i");
+        delIcon.className = "fas fa-trash-alt";
+        bb.append(delIcon);
         p.appendChild(aa);
         p.appendChild(bb);
-        console.log(p);
+        //console.log(p);
+
+        const box2 = document.querySelector(".box2");
+        box2.appendChild(p);
+
+        totalMoney.textContent = totalDollar;
+
+        let delBTN = document.querySelectorAll(".delBTN");
+        delBTN.forEach(del => {
+            del.addEventListener("click", function(){
+                // console.log("Delete Button Working SuccessFully.");
+                let price = this.previousElementSibling;
+                let lossPrice = parseFloat(price.getAttribute("doller"));
+                console.log(`Total: ${totalDollar} and lossVal: ${lossPrice}`);
+                totalDollar = totalDollar - lossPrice;
+                totalMoney.textContent = totalDollar;
+                this.parentElement.remove();
+            }); 
+        });
     }
 });
-
-
-				let total = 0;
-				$(".btn").click(function(){
-					
-					let a = $("#content").val();
-					let b = parseFloat($("#amount").val());
-					
-					if(a === "" || isNaN(b)){
-						return false;
-					}
-					$(".box2").prepend(`<p><aa>${a + ": $" + b.toFixed(2)}</aa><bb>Delete</bb></p>`);
-					total += b;
-                    $(".span1").text(total.toFixed(2));
-					$("#content, #amount").val("");
-					$("#content").focus();
-					
-					
-				});
-				
-				$(".box2").on("click", "bb", function() {
-				let amountText = $(this).siblings('aa').text(); 
-                let amountValue = parseFloat(amountText.split('$')[1]); 
-
-              
-                total -= amountValue;
-                $(".span1").text(total.toFixed(2)); 
-
-					$(this).closest("p").remove();
-                });
                 
